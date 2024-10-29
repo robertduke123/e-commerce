47c922f396f3a5b8928b3394267ce0f08a7e18d1
Robert Duke <robduke123@gmail.com>
2024-10-29 22:57:02 +1300

    firestore collection uploaded

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -8,7 +8,14 @@ import {
 	signOut,
 	onAuthStateChanged,
 } from "firebase/auth";
-import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+import {
+	getFirestore,
+	doc,
+	getDoc,
+	setDoc,
+	collection,
+	writeBatch,
+} from "firebase/firestore";
 
 const firebaseConfig = {
 	apiKey: "AIzaSyA0u6-hv_f71oXMA7TnjjCMK9iQeiJO_bU",
@@ -31,6 +38,20 @@ export const signInWithGooglePopup = () =>
 	signInWithPopup(auth, googleProvider);
 
 export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (
+	collectionKey,
+	objectsToAdd
+) => {
+	const collectionRef = collection(db, collectionKey);
+	const batch = writeBatch(db);
+	objectsToAdd.forEach((object) => {
+		const docRef = doc(collectionRef, object.title.toLowerCase());
+		batch.set(docRef, object);
+	});
+	await batch.commit();
+	console.log("done");
+};
 
 export const creatUserDocumentFromAuth = async (
 	userAuth,
